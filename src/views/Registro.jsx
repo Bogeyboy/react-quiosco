58cb882f62b41a7e8bfb7c031052e6a190f2d222
Carlos Muñoz <carlos.munoz.rodriguez@gmail.com>
2024-07-09 14:05:08 +0200

    Clase 262

--- src/views/Registro.jsx
+++ src/views/Registro.jsx
@@ -1,5 +1,29 @@
 export default function Registro() {
     return (
-        <div>Registro</div>
+        /* <div>
+            <h1 className="text-4xl font-black">Crea tu cuenta de usuario</h1>
+            <p>Crea tu cuenta llenando el formulario</p>
+        </div> */
+        <>{/* Esto es un fragment, una manera de no añadir html extra al código */}
+            <h1 className="text-4xl font-black">Crea tu cuenta de usuario</h1>
+            <p>Crea tu cuenta llenando el formulario</p>
+            <div className="bg-whit shadow-md rounded-md mt-10 px-5 py-10">
+                <form>
+                    <div className="mb-4">
+                        <label
+                            htmlFor="name"
+                            className="text-slate-800"
+                        >Nombre:</label>
+                        <input
+                            id="name"
+                            name="name"
+                            type="text"
+                            className="mt-2 w-full p-3 bg-gray-50 border border-gray-400 rounded-lg"
+                            placeholder="Tu nombre"
+                        />
+                    </div>
+                </form>
+            </div>
+        </>
     )
 }
