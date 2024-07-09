76783b2613eb504133b379fdc18c9d62cb5c6b47
Carlos Muñoz <carlos.munoz.rodriguez@gmail.com>
2024-07-09 20:14:33 +0200

    Clase 266

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,10 +1,17 @@
 import { Outlet } from 'react-router-dom'
+import Sidebar from '../components/Sidebar'
+import Resumen from '../components/Resumen'
 
 export default function Layout() {
     return (
-    <div className="font-bold text-3xl">
-        Mostrando desde el layout
-        <Outlet />
+    <div className="md:flex">
+        <Sidebar />
+        
+        <main className="flex-1">
+            <Outlet />
+        </main>
+        
+        <Resumen />
     </div>
     )
 }
