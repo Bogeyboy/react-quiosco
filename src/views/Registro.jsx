3d79301348ee5b2897fbb597273a8b2194228241
Carlos Muñoz <carlos.munoz.rodriguez@gmail.com>
2024-07-09 14:20:24 +0200

    Clase 263

--- src/views/Registro.jsx
+++ src/views/Registro.jsx
@@ -9,6 +9,7 @@ export default function Registro() {
             <p>Crea tu cuenta llenando el formulario</p>
             <div className="bg-whit shadow-md rounded-md mt-10 px-5 py-10">
                 <form>
+                    {/* Nombre del usuario */}
                     <div className="mb-4">
                         <label
                             htmlFor="name"
@@ -18,10 +19,58 @@ export default function Registro() {
                             id="name"
                             name="name"
                             type="text"
-                            className="mt-2 w-full p-3 bg-gray-50 border border-gray-400 rounded-lg"
+                            className="mt-2 w-full p-3 bg-gray-50"
                             placeholder="Tu nombre"
                         />
                     </div>
+                    {/* Email del usuario */}
+                    <div className="mb-4">
+                        <label
+                            htmlFor="email"
+                            className="text-slate-800"
+                        >Email:</label>
+                        <input
+                            id="email"
+                            name="email"
+                            type="email"
+                            className="mt-2 w-full p-3 bg-gray-50"
+                            placeholder="Tu email"
+                        />
+                    </div>
+                    {/* Password del usuario */}
+                    <div className="mb-4">
+                        <label
+                            htmlFor="password"
+                            className="text-slate-800"
+                        >Password:</label>
+                        <input
+                            id="password"
+                            name="password"
+                            type="password"
+                            className="mt-2 w-full p-3 bg-gray-50"
+                            placeholder="Tu password"
+                        />
+                    </div>
+                    {/* Confirmación de Password del usuario */}
+                    <div className="mb-4">
+                        <label
+                            htmlFor="password_confirmation"
+                            className="text-slate-800"
+                        >Confirmar password:</label>
+                        <input
+                            id="password_confirmation"
+                            name="password_confirmation"
+                            type="password"
+                            className="mt-2 w-full p-3 bg-gray-50"
+                            placeholder="Repetir password"
+                        />
+                    </div>
+                    <input
+                        type="submit"
+                        value="Crear cuenta"
+                        className="bg-indigo-600 hover:bg-indigo-800 text-white w-full mt-5 p-3
+                            uppercase font-bold cursor-pointer"
+                        />
                 </form>
             </div>
         </>
