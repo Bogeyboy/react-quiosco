3ae4af56a5040fa1341ec2fa17838914c3e48d8f
Carlos Muñoz <carlos.munoz.rodriguez@gmail.com>
2024-07-23 19:23:24 +0200

    Clase 316

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,12 +1,40 @@
+import { createRef,useState } from 'react'
 import { Link } from 'react-router-dom'
+import Alerta from '../components/Alerta';
+import clienteAxios from '../config/axios'
 
 export default function Login() {
+
+    const emailRef = createRef();
+    const passwordRef = createRef();
+
+    const [errores, setErrores] = useState([]);
+    
+    //Evento creado para cuando se envía el formulario
+    const handleSubmit = async e => {
+        e.preventDefault();
+
+        const datos = {
+            email: emailRef.current.value,
+            password: passwordRef.current.value,
+        }
+        try {
+            const {data} = await clienteAxios.post('/api/login', datos);
+            console.log(data.token);
+        } catch (error) {
+            setErrores(Object.values(error.response.data.errors));
+        }
+    }
+
     return (
         <>{/* Esto es un fragment, una manera de no añadir html extra al código */}
             <h1 className="text-4xl font-black">Inicia sesión</h1>
             <p>Para crear un pedido debes iniciar sesión</p>
             <div className="bg-whit shadow-md rounded-md mt-10 px-5 py-10">
-                <form>
+                <form
+                    onSubmit={handleSubmit}
+                    noValidate
+                >
                     {/* Email del usuario */}
                     <div className="mb-4">
                         <label
@@ -19,6 +47,7 @@ export default function Login() {
                             type="email"
                             className="mt-2 w-full p-3 bg-gray-50"
                             placeholder="Tu email"
+                            ref={emailRef}
                         />
                     </div>
                     {/* Password del usuario */}
@@ -33,6 +62,7 @@ export default function Login() {
                             type="password"
                             className="mt-2 w-full p-3 bg-gray-50"
                             placeholder="Tu password"
+                            ref={passwordRef}
                         />
                     </div>
                     {/* Botón para iniciar sesion */}
