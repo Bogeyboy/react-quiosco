35defb369a4af36855a156fa5f521eeee4bd88c5
Carlos Muñoz <carlos.munoz.rodriguez@gmail.com>
2024-07-26 08:43:12 +0200

    Clase 321

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,12 +1,29 @@
+import useSWR from "swr";
 import clienteAxios from "../config/axios";
 
 export const useAuth = ({middleware, url}) => {
 
+    const token = localStorage.getItem('AUTH_TOKEN');//Obtenemos el token de localStorage
+    
+    //Llamada a cliente Axios
+    const {data: user, error, mutate} = useSWR('/api/user', ()=>
+        clienteAxios('/api/user',{
+            headers:{
+                Authorization: `Bearer ${token}`
+            }
+        })
+        .then(res => res.data)
+        .catch(error => {
+            throw Error(error?.response?.data?.errors);
+        })
+    );
+    
     const login = async (datos, setErrores) => {
         try {
             const {data} = await clienteAxios.post('/api/login', datos);
             localStorage.setItem('AUTH_TOKEN', data.token);
             setErrores([]);
+            await mutate(); 
         } catch (error) {
             setErrores(Object.values(error.response.data.errors));
         }
@@ -19,6 +36,9 @@ export const useAuth = ({middleware, url}) => {
     const logout = () => {
         
     }
+
+    console.log(user);
+    console.log(error);
 
     return {
         login,
