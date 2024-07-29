3125e9548acc212a68b93b75c44696ad16d4d756
Carlos Muñoz <carlos.munoz.rodriguez@gmail.com>
2024-07-29 18:17:32 +0200

    Clase 339

--- src/views/Ordenes.jsx
+++ src/views/Ordenes.jsx
@@ -10,20 +10,53 @@ export default function Ordenes() {
             Authorization: `Bearer ${token}`
         }
     })
-    const {data, error, isLoading} =  useSWR('/api/pedidos', fetcher);
+    const {data, error, isLoading} =  useSWR('/api/pedidos', fetcher/* , {refreshInterval:1000} */);
 
-    console.log(data);
-    console.log(error);
-    console.log(isLoading);
-
+    if (isLoading) return 'Cargando las órdenes......'
+    
     return (
+        
         <div>
+            {/* Div de cabecera con texto */}
             <h1 className="text-4xl font-black">
                 Órdenes
             </h1>
             <p className="text-2xl my-10">
                 Administra las órdenes desde aquí
             </p>
+            {/* Div en el que se mostrarán las órdenes */}
+            <div>
+                {data.data.data.map(pedido => (
+                    <div key={pedido.id} className="p-5 bg-white shawod space-y-2 border-b">
+                        <p className="text-xl font-bold text-slate-600">
+                            Contenido del pedido: {pedido.id}
+                        </p>
+
+                        {/* {if} */}
+                        {pedido.productos.map(producto => (
+                            <div
+                                key={producto.id}
+                                className="border-b border-b-slate-200 last-of-type:border-none py-4"
+                            >
+                                {/* <p className="text-sm">
+                                    ID: {producto.id}
+                                </p> */}
+                                <p>
+                                    {producto.nombre}
+                                </p>
+                                <p className="text-sm">
+                                    Cantidad: {''}
+                                    <span className="font-bold">
+                                        {producto.pivot.cantidad}
+                                    </span>
+                                </p>
+                            </div>
+                        ))}
+                    </div>
+                ))}
+            </div>
         </div>
+        //Div donde vamos a mostrar las ordenes
+        
     )
 }
